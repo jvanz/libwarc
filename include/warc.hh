d5f345462ff93e718a8ff73e3bfe2fb77815d7ae
José Guilherme Vanz <guilherme.sft@gmail.com>
2017-11-09 13:38:38 -0200

    warc.hh: WARCField template class
    Adds a simple class to represent a field in a WARC record
    
    Signed-off-by: José Guilherme Vanz <guilherme.sft@gmail.com>

--- include/warc.hh
+++ include/warc.hh
@@ -3,11 +3,20 @@
 #include <string>
 #include <istream>
 
-namespace warc{
+using namespace std;
 
+namespace warc { inline namespace v1 { 
+
+template <typename T>
 class WARCField
 {
-	// TODO
+	private:
+		string name;
+		T value;
+	public:
+		explicit WARCField(string name, T value): name{name}, value{value} {};
+		inline string get_name() const { return this->name; };
+		inline T get_value() const { return this->value; };
 };
 
-}
+} }
