a835771a31dc78c7ef1df0c52fdd33119bd40f69
José Guilherme Vanz <guilherme.sft@gmail.com>
2017-11-09 13:39:25 -0200

    tests: simple WARCField test
    Adds a simple test to validate the WARCField object initialization.
    
    Signed-off-by: José Guilherme Vanz <guilherme.sft@gmail.com>

--- tests/main.cc
+++ tests/main.cc
@@ -3,6 +3,16 @@
 
 #include "warc.hh"
 
+using namespace warc;
+using namespace std;
+
+TEST(WARCField, get_name) 
+{
+	string name("myfield");
+	WARCField<string> field(name, "value");
+	ASSERT_EQ(name, field.get_name());
+}
+
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
