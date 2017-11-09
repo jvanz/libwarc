81a54d94d487863b7c553a32d68b7ca16f149d93
José Guilherme Vanz <guilherme.sft@gmail.com>
2017-11-09 21:37:44 -0200

    tests: test to check WARCField value field
    Adds a test case to validate the value field of the WARCField class
    
    Signed-off-by: José Guilherme Vanz <guilherme.sft@gmail.com>

--- tests/main.cc
+++ tests/main.cc
@@ -13,6 +13,21 @@ TEST(WARCField, get_name)
 	ASSERT_EQ(name, field.get_name());
 }
 
+TEST(WARCField, get_value) 
+{
+	WARCField<string> field("name", "value");
+	ASSERT_EQ("name", field.get_name());
+	ASSERT_EQ("value", field.get_value());
+	
+	WARCField<int> field2("name", 1);
+	ASSERT_EQ("name", field2.get_name());
+	ASSERT_EQ(1, field2.get_value());
+	
+	WARCField<float> field3("name", 9.9);
+	ASSERT_EQ("name", field3.get_name());
+	ASSERT_FLOAT_EQ(9.9, field3.get_value());
+}
+
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
