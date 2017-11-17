74eb7daca393f3214b63f396fa0b15d2cae4eb7e
José Guilherme Vanz <guilherme.sft@gmail.com>
2017-11-16 23:54:37 -0200

    warc.hh: adds WARCField class
    Adds a class to store the header field's name and value. An operator<<
    has also been added to print the object nicely.
    
    Signed-off-by: José Guilherme Vanz <guilherme.sft@gmail.com>

--- include/warc.hh
+++ include/warc.hh
@@ -8,6 +8,30 @@ using namespace std;
 
 namespace warc { inline namespace v1 { 
 
+template<typename T>
+class WARCField
+{
+	std::string name;
+	T value;
+	public:
+		explicit WARCField(string _name, T _value) : name{_name}, 
+			 value{_value} { }
+		// move constructor
+		WARCField(WARCField&& r) : name(std::move(r.name)), 
+			value(std::move(r.value)) { };
+	template<typename P>
+	friend std::ostream& operator<<(std::ostream& os, const WARCField<P>& field);
+};
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const WARCField<T>& field)
+{
+	std::ostream::sentry s(os);
+	if (s) 
+		os << field.name << ": " << field.value;
+	return os;
+};
+
 class WARCRecord
 {
 	private:
