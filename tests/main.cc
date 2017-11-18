f4f9c9068431d8ba4eed677d0c419a7ad1dac3c5
José Guilherme Vanz <guilherme.sft@gmail.com>
2017-11-18 18:56:16 -0200

    tests: add tests to validate the warc record parsing
    Adds test case to test if the function to parse a input stream into a
    WARCRecord object is parsing the WARC fields properly. Issue #2
    
    Signed-off-by: José Guilherme Vanz <guilherme.sft@gmail.com>

--- tests/main.cc
+++ tests/main.cc
@@ -1,4 +1,3 @@
-
 #include <gtest/gtest.h>
 
 #include "warc.hh"
@@ -6,26 +5,62 @@
 using namespace warc;
 using namespace std;
 
-TEST(WARCField, get_name) 
+static const char CR= 0x0D;
+static const char LF= 0x0A;
+
+static std::stringstream get_record()
 {
-	string name("myfield");
-	WARCField<string> field(name, "value");
-	ASSERT_EQ(name, field.get_name());
+	std::stringstream ss;
+	ss << "WARC/1.0" << CR << LF;
+	ss << "WARC-Type: warcinfo" << CR << LF;
+	ss << "WARC-Date: 2017-10-24T10:57:36Z" << CR << LF;
+	ss << "WARC-Record-ID: <urn:uuid:e98258fc-8fcf-4800-bdb7-e02e19275f27>" << CR << LF;
+	ss << "Content-Length: 329" << CR << LF;
+	ss << "Content-Type: application/warc-fields" << CR << LF;
+	ss << "WARC-Filename: CC-MAIN-20171024105736-20171024125736-00899.warc.gz" << CR << LF;
+	ss << CR << LF;
+	ss << "robots: classic" << CR << LF;
+	ss << "hostname: ip-10-203-171-252.ec2.internal" << CR << LF;
+	ss << "software: Nutch 1.6 (CC)" << CR << LF;
+	ss << "isPartOf: CC-MAIN-2017-43" << CR << LF;
+	ss << "operator: Common Crawl Admin" << CR << LF;
+	ss << "description: Wide crawl of the web for October 2017" << CR << LF;
+	ss << "publisher: Common Crawl" << CR << LF;
+	ss << "format: WARC File Format 1.0" << CR << LF;
+	ss << "conformsTo: http://bibnum.bnf.fr/WARC/WARC_ISO_28500_version1_latestdraft.pdf" << CR << LF;
+	return ss;
 }
 
-TEST(WARCField, get_value) 
+TEST(WARCRecord, parse_warc_record) 
 {
-	WARCField<string> field("name", "value");
-	ASSERT_EQ("name", field.get_name());
-	ASSERT_EQ("value", field.get_value());
-	
-	WARCField<int> field2("name", 1);
-	ASSERT_EQ("name", field2.get_name());
-	ASSERT_EQ(1, field2.get_value());
-	
-	WARCField<float> field3("name", 9.9);
-	ASSERT_EQ("name", field3.get_name());
-	ASSERT_FLOAT_EQ(9.9, field3.get_value());
+	auto rstream = get_record();
+	auto record = parse_warc_record(rstream);
+	ASSERT_EQ(6, record.get_fields().size()) 
+		<< "There are more fields than expected";
+	for (auto field : record.get_fields()){
+		if (field.get_name() == "WARC-Type")
+			ASSERT_EQ("warcinfo", field.get_value()) 
+				<< "Invalid WARC-Type value";
+		else if (field.get_name() == "WARC-Date")
+			ASSERT_EQ("2017-10-24T10:57:36Z", field.get_value()) 
+				<< "Invalid WARC-Date value";
+		else if (field.get_name() == "WARC-Record-ID")
+			ASSERT_EQ("<urn:uuid:e98258fc-8fcf-4800-bdb7-e02e19275f27>", field.get_value()) 
+				<< "Invalid WARC-Record-ID value";
+		else if (field.get_name() == "Content-Length")
+			ASSERT_EQ("329", field.get_value()) 
+				<< "Invalid Contet-Length value";
+		else if (field.get_name() == "Content-Type")
+			ASSERT_EQ("application/warc-fields", field.get_value()) 
+				<< "Invalid Content-Type value";
+		else if (field.get_name() == "WARC-Filename")
+			ASSERT_EQ("CC-MAIN-20171024105736-20171024125736-00899.warc.gz", field.get_value()) 
+				<< "Invalid WARC-Filename value";
+		else
+			FAIL() << "Unknown field: " << field.get_name() << ": " 
+				<< field.get_value();
+
+	}
 }
 
 int main(int argc, char **argv) {
