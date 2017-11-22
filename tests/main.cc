22d7d20888eaa2ff4532375e045f69511eac159e
José Guilherme Vanz <guilherme.sft@gmail.com>
2017-11-21 23:19:09 -0200

    test: check parsing within istream_iterator
    Add a new test case to validate if the parsing works fine when used with
    an istream_iterator.
    
    Signed-off-by: José Guilherme Vanz <guilherme.sft@gmail.com>

--- tests/main.cc
+++ tests/main.cc
@@ -1,4 +1,7 @@
 #include <gtest/gtest.h>
+#include <algorithm>
+#include <sstream>
+#include <type_traits>
 
 #include "warc.hh"
 
@@ -7,7 +10,7 @@ using namespace std;
 
 static const char CR= 0x0D;
 static const char LF= 0x0A;
-	
+
 static std::stringstream get_invalid_field_name_record()
 {
 	std::stringstream ss;
@@ -65,43 +68,86 @@ static std::stringstream get_record()
 }
 
 
-TEST(WARCRecord, parse_warc_record) 
+TEST(WARCRecord, parse_warc_record)
 {
 	auto rstream = get_record();
-	auto record = parse_warc_record(rstream);
-	ASSERT_EQ(6, record.get_fields().size()) 
+	WARCRecord record;
+	rstream >> record;
+	ASSERT_EQ(6, record.get_fields().size())
 		<< "There are more fields than expected";
 	for (auto field : record.get_fields()){
 		if (field.get_name() == "WARC-Type")
-			ASSERT_EQ("warcinfo", field.get_value()) 
+			ASSERT_EQ("warcinfo", field.get_value())
 				<< "Invalid WARC-Type value";
 		else if (field.get_name() == "WARC-Date")
-			ASSERT_EQ("2017-10-24T10:57:36Z", field.get_value()) 
+			ASSERT_EQ("2017-10-24T10:57:36Z", field.get_value())
 				<< "Invalid WARC-Date value";
 		else if (field.get_name() == "WARC-Record-ID")
-			ASSERT_EQ("<urn:uuid:e98258fc-8fcf-4800-bdb7-e02e19275f27>", field.get_value()) 
+			ASSERT_EQ("<urn:uuid:e98258fc-8fcf-4800-bdb7-e02e19275f27>", field.get_value())
 				<< "Invalid WARC-Record-ID value";
 		else if (field.get_name() == "Content-Length")
-			ASSERT_EQ("329", field.get_value()) 
+			ASSERT_EQ("329", field.get_value())
 				<< "Invalid Contet-Length value";
 		else if (field.get_name() == "Content-Type")
-			ASSERT_EQ("application/warc-fields", field.get_value()) 
+			ASSERT_EQ("application/warc-fields", field.get_value())
 				<< "Invalid Content-Type value";
 		else if (field.get_name() == "WARC-Filename")
-			ASSERT_EQ("CC-MAIN-20171024105736-20171024125736-00899.warc.gz", field.get_value()) 
+			ASSERT_EQ("CC-MAIN-20171024105736-20171024125736-00899.warc.gz", field.get_value())
 				<< "Invalid WARC-Filename value";
 		else
-			FAIL() << "Unknown field: " << field.get_name() << ": " 
+			FAIL() << "Unknown field: " << field.get_name() << ": "
 				<< field.get_value();
 	}
 	ASSERT_EQ(get_record_content().str().size(), record.get_content().size());
 	ASSERT_EQ(get_record_content().str(), record.get_content());
 }
 
-TEST(WARCRecord, invalid_field_name) 
+TEST(WARCRecord, invalid_field_name)
 {
 	auto rstream = get_invalid_field_name_record();
-	ASSERT_ANY_THROW(parse_warc_record(rstream));
+	WARCRecord record;
+	ASSERT_ANY_THROW(rstream >> record);
+}
+
+TEST(WARCRecord, warc_record_input_iterator)
+{
+	std::stringstream record;
+	record << get_record().str();
+	record << get_record().str();
+	record << get_record().str();
+	std::for_each(std::istream_iterator<WARCRecord>(record),
+		std::istream_iterator<WARCRecord>(),
+		[this] (const WARCRecord& record) {
+			ASSERT_EQ(6, record.get_fields().size())
+				<< "There are more fields than expected";
+			for (auto field : record.get_fields()){
+				if (field.get_name() == "WARC-Type")
+					ASSERT_EQ("warcinfo", field.get_value())
+						<< "Invalid WARC-Type value";
+				else if (field.get_name() == "WARC-Date")
+					ASSERT_EQ("2017-10-24T10:57:36Z", field.get_value())
+						<< "Invalid WARC-Date value";
+				else if (field.get_name() == "WARC-Record-ID")
+					ASSERT_EQ("<urn:uuid:e98258fc-8fcf-4800-bdb7-e02e19275f27>", field.get_value())
+						<< "Invalid WARC-Record-ID value";
+				else if (field.get_name() == "Content-Length")
+					ASSERT_EQ("329", field.get_value())
+						<< "Invalid Contet-Length value";
+				else if (field.get_name() == "Content-Type")
+					ASSERT_EQ("application/warc-fields", field.get_value())
+						<< "Invalid Content-Type value";
+				else if (field.get_name() == "WARC-Filename")
+					ASSERT_EQ("CC-MAIN-20171024105736-20171024125736-00899.warc.gz", field.get_value())
+						<< "Invalid WARC-Filename value";
+				else
+					FAIL() << "Unknown field: " << field.get_name() << ": "
+						<< field.get_value();
+			}
+			ASSERT_EQ(get_record_content().str().size(), record.get_content().size());
+			ASSERT_EQ(get_record_content().str(), record.get_content());
+		});
+}
+
 }
 
 int main(int argc, char **argv) {
