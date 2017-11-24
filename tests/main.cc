c8027f43b5db7cd49efd61211f35554efc9d13e3
José Guilherme Vanz <guilherme.sft@gmail.com>
2017-11-23 23:47:41 -0200

    tests: use helper functions in the test cases
    Updates the test cases to use the helpers functions to create dummy
    input data to test the library
    
    Signed-off-by: José Guilherme Vanz <guilherme.sft@gmail.com>

--- tests/main.cc
+++ tests/main.cc
@@ -4,75 +4,34 @@
 #include <type_traits>
 
 #include "warc.hh"
+#include "helper.hh"
 
 using namespace warc;
 using namespace std;
-
-static const char CR= 0x0D;
-static const char LF= 0x0A;
-
-static std::stringstream get_invalid_field_name_record()
-{
-	std::stringstream ss;
-	ss << "WARC/1.0" << CR << LF;
-	ss << "WARC-Type: warcinfo" << CR << LF;
-	ss << "WARC-Date: 2017-10-24T10:57:36Z" << CR << LF;
-	ss << "WARC-Record-ID: <urn:uuid:e98258fc-8fcf-4800-bdb7-e02e19275f27>" << CR << LF;
-	ss << "Content-Length: 329" << CR << LF;
-	ss << "Content-Type: application/warc-fields" << CR << LF;
-	ss << "Invalid-Field: CC-MAIN-20171024105736-20171024125736-00899.warc.gz" << CR << LF;
-	ss << CR << LF;
-	ss << "robots: classic" << CR << LF;
-	ss << "hostname: ip-10-203-171-252.ec2.internal" << CR << LF;
-	ss << "software: Nutch 1.6 (CC)" << CR << LF;
-	ss << "isPartOf: CC-MAIN-2017-43" << CR << LF;
-	ss << "operator: Common Crawl Admin" << CR << LF;
-	ss << "description: Wide crawl of the web for October 2017" << CR << LF;
-	ss << "publisher: Common Crawl" << CR << LF;
-	ss << "format: WARC File Format 1.0" << CR << LF;
-	ss << "conformsTo: http://bibnum.bnf.fr/WARC/WARC_ISO_28500_version1_latestdraft.pdf" << CR << LF;
-	ss << CR << LF;
-	ss << CR << LF;
-	ss << CR << LF;
-	return ss;
-}
-
-static std::stringstream get_record_content()
-{
-	std::stringstream ss;
-	ss << "robots: classic" << CR << LF;
-	ss << "hostname: ip-10-203-171-252.ec2.internal" << CR << LF;
-	ss << "software: Nutch 1.6 (CC)" << CR << LF;
-	ss << "isPartOf: CC-MAIN-2017-43" << CR << LF;
-	ss << "operator: Common Crawl Admin" << CR << LF;
-	ss << "description: Wide crawl of the web for October 2017" << CR << LF;
-	ss << "publisher: Common Crawl" << CR << LF;
-	ss << "format: WARC File Format 1.0" << CR << LF;
-	ss << "conformsTo: http://bibnum.bnf.fr/WARC/WARC_ISO_28500_version1_latestdraft.pdf" << CR << LF;
-	return ss;
-}
-
-static std::stringstream get_record()
-{
-	std::stringstream ss;
-	ss << "WARC/1.0" << CR << LF;
-	ss << "WARC-Type: warcinfo" << CR << LF;
-	ss << "WARC-Date: 2017-10-24T10:57:36Z" << CR << LF;
-	ss << "WARC-Record-ID: <urn:uuid:e98258fc-8fcf-4800-bdb7-e02e19275f27>" << CR << LF;
-	ss << "Content-Length: 329" << CR << LF;
-	ss << "Content-Type: application/warc-fields" << CR << LF;
-	ss << "WARC-Filename: CC-MAIN-20171024105736-20171024125736-00899.warc.gz" << CR << LF;
-	ss << CR << LF;
-	ss << get_record_content().str();
-	return ss;
-}
-
 
 TEST(WARCRecord, parse_warc_record)
 {
 	auto rstream = get_record();
 	WARCRecord record;
 	rstream >> record;
+	assert_record(record);
+}
+
+TEST(WARCRecord, invalid_field_name)
+{
+	auto rstream = get_invalid_field_name_record();
+	WARCRecord record;
+	ASSERT_ANY_THROW(rstream >> record);
+}
+
+TEST(WARCRecord, warc_record_input_iterator)
+{
+	std::stringstream records;
+	records << get_record().str();
+	records << get_record2().str();
+	//std::istream_iterator<WARCRecord> it(records);
+	WARCRecord record;
+	records >> record;
 	ASSERT_EQ(6, record.get_fields().size())
 		<< "There are more fields than expected";
 	for (auto field : record.get_fields()){
@@ -98,56 +57,66 @@ TEST(WARCRecord, parse_warc_record)
 			FAIL() << "Unknown field: " << field.get_name() << ": "
 				<< field.get_value();
 	}
-	ASSERT_EQ(get_record_content().str().size(), record.get_content().size());
 	ASSERT_EQ(get_record_content().str(), record.get_content());
+	ASSERT_EQ(get_record_content().str().size(), record.get_content().size());
+	//it++;
+	WARCRecord record2;
+	records >> record2;
+	std::cout << record2 << std::endl;
+	ASSERT_EQ(8, record2.get_fields().size())
+		<< "There are more fields than expected";
+	for (auto field : record2.get_fields()){
+		if (field.get_name() == "WARC-Type")
+			ASSERT_EQ("request", field.get_value())
+				<< "Invalid WARC-Type value";
+		else if (field.get_name() == "WARC-Date")
+			ASSERT_EQ("2017-10-24T11:29:43Z", field.get_value())
+				<< "Invalid WARC-Date value";
+		else if (field.get_name() == "WARC-Record-ID")
+			ASSERT_EQ("<urn:uuid:5c576cdd-5402-446a-b3c1-03f25b4c45b4>", field.get_value())
+				<< "Invalid WARC-Record-ID value";
+		else if (field.get_name() == "Content-Length")
+			ASSERT_EQ("287", field.get_value())
+				<< "Invalid Contet-Length value";
+		else if (field.get_name() == "Content-Type")
+			ASSERT_EQ("application/http; msgtype=request", field.get_value())
+				<< "Invalid Content-Type value";
+		else if (field.get_name() == "WARC-Warcinfo-ID")
+			ASSERT_EQ("<urn:uuid:e98258fc-8fcf-4800-bdb7-e02e19275f27>", field.get_value())
+				<< "Invalid WARC-Warcinfo-ID value";
+		else if (field.get_name() == "WARC-IP-Address")
+			ASSERT_EQ("104.244.98.65", field.get_value())
+				<< "Invalid WARC-IP-Address value";
+		else if (field.get_name() == "WARC-Target-URI")
+			ASSERT_EQ("http://00iamgundam00.blog.fc2.com/blog-entry-935.html", field.get_value())
+				<< "Invalid WARC-Target-URI value";
+		else
+			FAIL() << "Unknown field: " << field.get_name() << ": "
+				<< field.get_value();
+	}
+	ASSERT_EQ(get_record2_content().str(), record2.get_content());
+	ASSERT_EQ(get_record2_content().str().size(), record2.get_content().size());
+
 }
 
-TEST(WARCRecord, invalid_field_name)
+TEST(WARCField, warcfield_cpp_concepts)
 {
-	auto rstream = get_invalid_field_name_record();
-	WARCRecord record;
-	ASSERT_ANY_THROW(rstream >> record);
+	ASSERT_EQ(true, std::is_copy_assignable<WARCField>::value)
+		<< "WARCField is not copy assignable";
+	ASSERT_EQ(true, std::is_copy_constructible<WARCField>::value)
+		<< "WARCField is not copy constructible";
+	ASSERT_EQ(true, std::is_default_constructible<WARCField>::value)
+		<< "WARCField is not default constructible";
 }
 
-TEST(WARCRecord, warc_record_input_iterator)
+TEST(WARCRecord, warcrecord_cpp_concepts)
 {
-	std::stringstream record;
-	record << get_record().str();
-	record << get_record().str();
-	record << get_record().str();
-	std::for_each(std::istream_iterator<WARCRecord>(record),
-		std::istream_iterator<WARCRecord>(),
-		[this] (const WARCRecord& record) {
-			ASSERT_EQ(6, record.get_fields().size())
-				<< "There are more fields than expected";
-			for (auto field : record.get_fields()){
-				if (field.get_name() == "WARC-Type")
-					ASSERT_EQ("warcinfo", field.get_value())
-						<< "Invalid WARC-Type value";
-				else if (field.get_name() == "WARC-Date")
-					ASSERT_EQ("2017-10-24T10:57:36Z", field.get_value())
-						<< "Invalid WARC-Date value";
-				else if (field.get_name() == "WARC-Record-ID")
-					ASSERT_EQ("<urn:uuid:e98258fc-8fcf-4800-bdb7-e02e19275f27>", field.get_value())
-						<< "Invalid WARC-Record-ID value";
-				else if (field.get_name() == "Content-Length")
-					ASSERT_EQ("329", field.get_value())
-						<< "Invalid Contet-Length value";
-				else if (field.get_name() == "Content-Type")
-					ASSERT_EQ("application/warc-fields", field.get_value())
-						<< "Invalid Content-Type value";
-				else if (field.get_name() == "WARC-Filename")
-					ASSERT_EQ("CC-MAIN-20171024105736-20171024125736-00899.warc.gz", field.get_value())
-						<< "Invalid WARC-Filename value";
-				else
-					FAIL() << "Unknown field: " << field.get_name() << ": "
-						<< field.get_value();
-			}
-			ASSERT_EQ(get_record_content().str().size(), record.get_content().size());
-			ASSERT_EQ(get_record_content().str(), record.get_content());
-		});
-}
-
+	ASSERT_EQ(true, std::is_copy_assignable<WARCRecord>::value)
+		<< "WARCRecord is not copy assignable";
+	ASSERT_EQ(true, std::is_copy_constructible<WARCRecord>::value)
+		<< "WARCRecord is not copy constructible";
+	ASSERT_EQ(true, std::is_default_constructible<WARCField>::value)
+		<< "WARCRecord is not default constructible";
 }
 
 int main(int argc, char **argv) {
